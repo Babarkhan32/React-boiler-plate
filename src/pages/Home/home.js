25c9e6643e0b13e9e8d373751726a38266a7ee6d
babarkhan32 <babar.0khan1@gmail.com>
2022-12-16 01:28:03 +0500

    removed redux from home

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,16 +1,8 @@
 import { useEffect } from "react";
 import { getUsers } from "../../store/actions/Home";
-import { useSelector, useDispatch } from "react-redux";
 
 export const Home = () => {
-  const dispatch = useDispatch();
-  const {
-    homeReducer: { data },
-  } = useSelector((store) => store);
-  console.log(data);
-  useEffect(() => {
-    dispatch(getUsers());
-  }, [dispatch]);
+  const data = [{ title: "New Title" }, { title: "New Drama" }];
   if (data.length === 0) {
     return <div>Loading.....</div>;
   }
